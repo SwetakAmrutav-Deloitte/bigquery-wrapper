5bdabcd11884084b931df03c9bd8e9c19d615eb3
Mohapatra <139158120SwetakAmrutav-Deloitte@users.noreply.github.com>
2023-07-19 18:22:47 +0530

    Test Class

--- bigquery/src/main/java/org/finos/legend/engine/bigquery/Connection.java
+++ bigquery/src/main/java/org/finos/legend/engine/bigquery/Connection.java
@@ -9,12 +9,15 @@ public class Connection extends BigQueryConnection{
 	
 	public static void main(String[] args) {
 		try {
-			Connection connection = new Connection(PROJECT_ID);
+			java.sql.Connection connection = new Connection(PROJECT_ID);
+			/*if (connection instanceof java.sql.Connection) {
+				System.out.println("True");
+			}
 			Statement statement = connection.createStatement();
 			String sql = "SELECT gameId FROM crmsm-278014.testlegendEngine.game_post_wide limit 10";
 			ResultSet rs = statement.executeQuery(sql);
 			String gameId = rs.getString("gemeId");
-			System.out.println(gameId);
+			System.out.println(gameId);*/
 		} catch (Exception e) {
 			System.out.println("Exception: " + e.getMessage());
 		}
---
+++ bigquery/src/main/java/org/finos/legend/engine/bigquery/connection/test/BigQueryConnectionTest.java
@@ -0,0 +1,10 @@
+package org.finos.legend.engine.bigquery.connection.test;
+
+import org.finos.legend.engine.bigquery.Connection;
+
+public class BigQueryConnectionTest {
+
+	public void testConnection(Connection connection, int repeat, String sqlExpression) {
+		
+	}
+}
